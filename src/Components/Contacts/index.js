62708fccc2a7739943992268b1bb9e73baf6e723
Rishab Lamba <rishablamba407@gmail.com>
2020-11-21 17:00:46 +0530

    basic styling

--- src/Components/Contacts/index.js
+++ src/Components/Contacts/index.js
@@ -11,26 +11,31 @@ const Contacts = () => {
                     imgSrc="http://vzkiss.com/demo/chatbox/images/avatar/avatar_1.jpg"
                     name="Sacha Griffin"
                     statusMessage="Super deep status message blah blah"
+                    lastMessage="2 mins"
                 />
                 <ContactListItem
                     imgSrc="http://vzkiss.com/demo/chatbox/images/avatar/avatar_2.jpeg"
                     name="Debby Jones"
                     statusMessage="New day, fresh start, fresh eadaf"
+                    lastMessage="5 mins"
                 />
                 <ContactListItem
                     imgSrc="http://vzkiss.com/demo/chatbox/images/avatar/avatar_3.jpg"
                     name="Sacha Griffin"
                     statusMessage="Life becomes more peaceful when"
+                    lastMessage="12 mins"
                 />
                 <ContactListItem
                     imgSrc="http://vzkiss.com/demo/chatbox/images/avatar/avatar_4.jpeg"
                     name="Johhny McGrump"
                     statusMessage="Please do not disturb"
+                    lastMessage="34 mins"
                 />
                 <ContactListItem
                     imgSrc="http://vzkiss.com/demo/chatbox/images/avatar/avatar_5.jpg"
                     name="Tommy Tom"
                     statusMessage="On vacation for two weeks"
+                    lastMessage="55 mins"
                 />
             </div>
         </div>
--- src/Components/Person/index.js
+++ src/Components/Person/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Person.css';
 
-const Person = ({ name, imgSrc, statusMessage }) => {
+const Person = ({ name, imgSrc, statusMessage, lastMessage }) => {
     return (
         <div className="person">
             <span className="person__avatar">
@@ -10,6 +10,7 @@ const Person = ({ name, imgSrc, statusMessage }) => {
             <span className="person__info">
                 <span className="person__name">{name}</span>
                 <span className="person__statusMsg">{statusMessage}</span>
+                <span class="lastMessage">{lastMessage}</span>
             </span>
         </div>
     );
